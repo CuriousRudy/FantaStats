133898e1ab61b5daa660b585815dc00872a79cf7
Abe Rudy <aberudy@gmail.com>
2017-10-23 18:11:39 -0400

    updated environment, database can be seeded.

--- config/environment.rb
+++ config/environment.rb
@@ -1,21 +1,5 @@
-require 'bundler/setup'
+require 'bundler'
 Bundler.require
 
-require 'rake'
-require 'active_record'
-require 'yaml/store'
-require 'ostruct'
-require 'require_all'
-
-DBNAME = "FantaStats"
-
-Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
-# Dir[File.join(File.dirname(__FILE__), "../lib/support", "*.rb")].each {|f| require f}
+ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
 require_all 'lib'
-
-DBRegistry["development"].connect!
-DB = ActiveRecord::Base.connection
-
-if ENV["ACTIVE_RECORD_ENV"] == "test"
-  ActiveRecord::Migration.verbose = false
-end
--- db/migrate/003_create_fantasy_team.rb
+++ db/migrate/003_create_fantasy_team.rb
@@ -1,4 +1,4 @@
-class CreateFantasyTeam < ActiveRecord::Migration
+class CreateFantasyTeam < ActiveRecord::Migration[4.2]
   def change
     create_table :fantasy_team do |t|
       t.string :name
