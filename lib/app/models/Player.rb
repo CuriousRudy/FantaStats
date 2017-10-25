bcf69cf2c2dc60a6c0724ca00dac70fad923ffca
github username <github email address>
2017-10-25 16:45:05 -0400

    add math methods and consistency rating

--- lib/app/models/Player.rb
+++ lib/app/models/Player.rb
@@ -1,6 +1,7 @@
 # Player model
 
 class Player < ActiveRecord::Base
+
   has_many :stats
   has_many :games, through: :stats
   belongs_to :fantasy_team
@@ -11,6 +12,7 @@ class Player < ActiveRecord::Base
     self.games.where(season: season)
   end
 
+  # total points accumulated over a season by a player, given the year
   def points_by_season(season) #works
     total = 0
     find_season_games(season).each do |game|
@@ -29,11 +31,37 @@ class Player < ActiveRecord::Base
     all_players_of_position_w_points.sort_nested_array
   end
 
+  def consistency_rating(season)
+    array_of_weekly_scores = []
+    self.find_season_games(season).each do |game|
+      array_of_weekly_scores << game.stats.find_by(player_id: self.id).score
+    end
+    binding.pry
+    standard_deviation(array_of_weekly_scores) / ppg_average(season)
+  end
+
 
   private
 
   def sort_nested_array(nested_array)
-    array.sort {|a, b| b[1] <=> a[1]}
+    nested_array.sort { |a, b| b[1] <=> a[1] }
+  end
+
+  def sum(array)
+    array.inject(0){|accum, i| accum + i }
+  end
+
+  def mean(array)
+    sum(array)/array.length.to_f
+  end
+
+  def sample_variance(array)
+    sum = array.inject(0){ |accum, i| accum + (i - mean(array)) ** 2 }
+    sum/(array.length - 1).to_f
+  end
+
+  def standard_deviation(array)
+    Math.sqrt(sample_variance(array))
   end
 
 end
